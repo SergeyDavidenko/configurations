1a124b25da5775a56cb50082997b65bb5a7a2757
Sergey Davidenko <sergey.davidenko@ardas.biz>
2019-12-27 16:44:12 +0200

    change example

--- example/main.go
+++ example/main.go
@@ -8,7 +8,7 @@ import (
 
 func init() {
 	url := "http://localhost:9971/auth2"
-	config, err := client.GetConfigFrom(url, "admin", "supersecret")
+	config, err := client.GetConfigFrom(url, "someuser", "supersecretpassword")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -17,10 +17,10 @@ func init() {
 }
 
 func main() {
-	prop := client.GetBoolOrDefault("blcache.kafka.enabled", true)
-	prop2 := client.GetIntOrDefault("blcache.sync.fixed.delay.ms", 111)
+	prop := client.GetBoolOrDefault("kafka.enabled", true)
+	prop2 := client.GetIntOrDefault("fixed.delay.ms", 111)
 	prop3 := client.GetStringOrDefault("test", "net")
-	prop4 := client.GetIntOrDefault("spring.redis.blcache.port", 6380)
+	prop4 := client.GetIntOrDefault("redis.port", 6380)
 	log.Println(prop)
 	log.Println(prop2)
 	log.Println(prop3)
